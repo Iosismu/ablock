070f841be140eeefeea8f6af2e8767d8ed386acf
Iosismu <evanpark333@gmail.com>
2021-07-19 00:07:24 +0900

    finish

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll(".nav-link");
 links.forEach((item) => {
   item.addEventListener("click", () => {
     let el = document.getElementById(item.getAttribute("data-link"));
-    el.scrollIntoView({ behavior: "smooth", block: "start" });
+    el.scrollIntoView({ behavior: "smooth", block: "start", inline: 'start'});
   });
 });
 
@@ -13,3 +13,27 @@ const menu = document.querySelector('.navbar_menu');
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
 })
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+// 부유하는(떠 다니는) 요소를 만드는 함수
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+      ease: Power1.easeInOut // Easing 함수 적용.
+    }
+  )
+}
+floatingObject('.dungdung', 1, 35)
+// floatingObject('.floating2', .5, 15)
+// floatingObject('.floating3', 1.5, 20)
